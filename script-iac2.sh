4a2f24a9ebc7050db7177844ad5bb9fd8f69f516
Dartanham <phanton3001@gmail.com>
2022-08-23 16:28:55 -0300

    Create script-iac2.sh

---
+++ script-iac2.sh
@@ -0,0 +1,14 @@
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/dartanham/linux_experience/archive/refs/heads/main.zip
+unzip main.zip
+cd linux_experience
+cp -R * /var/www/html/
